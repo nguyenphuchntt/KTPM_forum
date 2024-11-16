39f0203be3e3d585bae83db92e6ad6e9e38f951a
Hamza Maach <hamzamaach56@gmail.com>
2024-11-16 15:10:59 +0100

    get all data posts from the databese and display them in the home page

--- server/handlers/home.go
+++ server/handlers/home.go
@@ -1,45 +1,33 @@
 package handlers
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
-	"forum/server/models"
+	"forum/server/database/queries"
 	"forum/server/utils"
 )
 
-// Demo data for testing before the database is set up
-var (
-	content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum cumque, voluptas dolore veniam excepturi aspernatur vero atque! Temporibus suscipit, excepturi, id corporis quod ea aliquam sapiente vitae eos reiciendis fugit."
-	posts   = []models.Post{
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
-	}
-)
-
-func GetHome(w http.ResponseWriter, r *http.Request) {
+func GetHome(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.URL.Path != "/" {
-		utils.RenderError(w,r, http.StatusNotFound)
+		utils.RenderError(w, r, http.StatusNotFound)
 		return
 	}
 
 	if r.Method != http.MethodGet {
-		utils.RenderError(w,r, http.StatusMethodNotAllowed)
+		utils.RenderError(w, r, http.StatusMethodNotAllowed)
 		return
 	}
 
-	err := utils.RenderTemplate(w,r, "home", http.StatusOK, posts)
+	posts, err := queries.GetPosts(db)
+	if err != nil {
+		log.Println("Error fetching posts from the database:", err)
+		http.Redirect(w, r, "/500", http.StatusSeeOther)
+		return
+	}
+
+	err = utils.RenderTemplate(w, r, "home", http.StatusOK, posts)
 	if err != nil {
 		log.Println(err)
 		http.Redirect(w, r, "/500", http.StatusSeeOther)
