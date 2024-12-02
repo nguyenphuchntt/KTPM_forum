7c709b9a28093902ffd85dfa7c89d11ea2cfd03f
Hamza Maach <hamzamaach56@gmail.com>
2024-12-02 13:24:18 +0100

    update the frontend of the post creation form

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -9,24 +9,24 @@ function throttle(fn, delay) {
     };
 }
 
-const addcomment = throttle(addcomm,5000)
+const addcomment = throttle(addcomm, 5000)
 
 function postreaction(postId, reaction) {
-    document.getElementById("errorlogin"+postId).innerText = ``
+    document.getElementById("errorlogin" + postId).innerText = ``
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/post/postreaction", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
-            document.getElementById("likescount"+postId).innerHTML = `<i
+            document.getElementById("likescount" + postId).innerHTML = `<i
                     class="fa-regular fa-thumbs-up"></i>${response.likesCount}`;
-            document.getElementById("dislikescount"+postId).innerHTML = `<i
+            document.getElementById("dislikescount" + postId).innerHTML = `<i
                     class="fa-regular fa-thumbs-down"></i>${response.dislikesCount}`;
         } else if (xhr.status !== 200) {
-            document.getElementById("errorlogin"+postId).innerText = `You must login first!`
+            document.getElementById("errorlogin" + postId).innerText = `You must login first!`
             setTimeout(() => {
-                document.getElementById("errorlogin"+postId).innerText = ``
+                document.getElementById("errorlogin" + postId).innerText = ``
             }, 1000);
         }
     };
@@ -49,7 +49,7 @@ function commentreaction(commentid, reaction) {
             setTimeout(() => {
                 document.getElementById("commenterrorlogin" + commentid).innerText = ``
             }, 1000);
-           
+
         }
     };
     xhr.send(`reaction=${reaction}&comment_id=${commentid}`);
@@ -68,35 +68,68 @@ function addcomm(postId) {
             comment.innerHTML = `
                  <div class="comment">
             <div class="comment-header">
-                <p class="comment-user">`+response.username+`</p>
+                <p class="comment-user">`+ response.username + `</p>
                 <span></span>
-                <p class="comment-time">`+response.created_at+` </p>
+                <p class="comment-time">`+ response.created_at + ` </p>
             </div>
             <div class="comment-body">
-                <p class="comment-content">`+response.content+` </p>
+                <p class="comment-content">`+ response.content + ` </p>
             </div>
             <div class="comment-footer">
-                <button id="commentlikescount`+response.ID+`" onclick="commentreaction('`+response.ID+`','like')"
-                    class="comment-like"><i class="fa-regular fa-thumbs-up"></i>`+response.likes+`</button>
-                <button id="commentdislikescount`+response.ID+`" onclick="commentreaction('`+response.ID+`','dislike')"
-                    class="comment-dislike"><i class="fa-regular fa-thumbs-down"></i>`+response.dislikes+`</button>
+                <button id="commentlikescount`+ response.ID + `" onclick="commentreaction('` + response.ID + `','like')"
+                    class="comment-like"><i class="fa-regular fa-thumbs-up"></i>`+ response.likes + `</button>
+                <button id="commentdislikescount`+ response.ID + `" onclick="commentreaction('` + response.ID + `','dislike')"
+                    class="comment-dislike"><i class="fa-regular fa-thumbs-down"></i>`+ response.dislikes + `</button>
             </div>
-            <span style="color:red" id="commenterrorlogin`+response.ID+`"></span>
+            <span style="color:red" id="commenterrorlogin`+ response.ID + `"></span>
         </div>
                 `
             document.getElementsByClassName("comments")[0].prepend(comment)
-            document.getElementsByClassName("post-comments")[0].innerHTML = `<i class="fa-regular fa-comment"></i>`+response.commentscount
+            document.getElementsByClassName("post-comments")[0].innerHTML = `<i class="fa-regular fa-comment"></i>` + response.commentscount
         } else if (xhr.status === 400) {
-            document.getElementById("errorlogin"+postId).innerText = `Invalid comment!`
+            document.getElementById("errorlogin" + postId).innerText = `Invalid comment!`
             setTimeout(() => {
-                document.getElementById("errorlogin"+postId).innerText = ``
+                document.getElementById("errorlogin" + postId).innerText = ``
             }, 1000);
         } else if (xhr.status === 401) {
-            document.getElementById("errorlogin"+postId).innerText = `You must login first!`
+            document.getElementById("errorlogin" + postId).innerText = `You must login first!`
             setTimeout(() => {
-                document.getElementById("errorlogin"+postId).innerText = ``
+                document.getElementById("errorlogin" + postId).innerText = ``
             }, 1000);
         }
     };
     xhr.send(`postid=${postId}&comment=${content.value}`);
 }
+
+const select = document.getElementById('categories-select');
+
+select.addEventListener('change', (e) => {
+    // Parse the value as JSON to extract id and label
+    const selectedValue = JSON.parse(e.target.value);
+    const { id, label } = selectedValue;
+    // console.log('ID:', id, 'Label:', label);
+
+    // create the elemenet for the category
+    const span = document.createElement('span');
+    span.textContent = label;
+    span.classList.add('selected-category');
+
+
+    // create hidden input to hold the id of selected category
+    const input = document.createElement('input')
+    input.type = 'hidden';
+    input.value = id
+    input.name = 'categories'
+
+    // add the elements (span and hidden input) 
+    // at the first  position of the categories container
+    const categoriesContainer = document.querySelector('.selected-categories');
+    categoriesContainer.append(input, span);
+
+
+    // disable the option selected in the select
+    e.target.options[e.target.selectedIndex].disabled = true;
+
+    // Reset the select 
+    e.target.selectedIndex = 0;
+});
