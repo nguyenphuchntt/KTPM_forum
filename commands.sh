74cf92fb96b58e8d8c919bcc53b51812fc66d117
Hamza Maach <hamzamaach56@gmail.com>
2024-12-16 16:36:21 +0100

    Dockerize the project

---
+++ commands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Stop and remove existing container if it exists
+docker stop forum-con 
+docker rm forum-con
+
+# Remove unused data
+docker system prune -f
+
+# Remove the old image
+docker rmi forum-img 
+
+# Build a new image
+docker build --no-cache -f Dockerfile -t forum-img .
+
+# Run a new container
+docker run -d -p 8080:8080 --name forum-con forum-img
+
+docker logs forum-con
