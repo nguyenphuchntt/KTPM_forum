36d429808bdb68720acefd94a4af8bd5d8d030a3
Nguyen Van Phuc <nguyenvanphucuet@gmail.com>
2025-11-24 15:29:54 +0700

    init retry config

---
+++ server/utils/retry/calculate_delay.go
@@ -0,0 +1,33 @@
+package retry
+
+import (
+	"math"
+	"time"
+	"forum/server/config"
+)
+
+func CalculateDelay(cfg config.RetryConfig, attempt int) time.Duration {
+	if attempt <= 0 {
+		return 0
+	}
+
+	var delay time.Duration
+
+	switch cfg.Strategy {
+	case config.FixedDelay:
+		delay = cfg.InitialDelay
+	case config.LinearBackoff:
+		delay = time.Duration(attempt) * cfg.InitialDelay
+	case config.ExponentialBackoff:
+		multiplier := math.Pow(2, float64(attempt-1))
+		delay = time.Duration(float64(cfg.InitialDelay) * multiplier)
+	default:
+		delay = cfg.InitialDelay
+	}
+
+	if delay > cfg.MaxDelay {
+		delay = cfg.MaxDelay
+	}
+
+	return delay
+}
